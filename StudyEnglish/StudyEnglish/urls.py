263823690a6c926330801035502940289d57de83
Ruslan Shevyrev <ruslan.shevyrev@gmail.com>
2022-10-10 21:25:14 +0300

    add app posts urls

--- StudyEnglish/StudyEnglish/urls.py
+++ StudyEnglish/StudyEnglish/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('posts/', include('posts.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
